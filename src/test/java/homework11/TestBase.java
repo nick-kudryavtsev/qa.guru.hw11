1aad8d5e5d6eaaee267eb32077b435c35dc9b4d5
nick-kudryavtsev <nick_kudryavtsev@internet.ru>
2022-10-10 12:46:39 +0300

    attach video added

--- src/test/java/homework11/TestBase.java
+++ src/test/java/homework11/TestBase.java
@@ -4,12 +4,18 @@ import com.codeborne.selenide.Configuration;
 import homework11.helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     @BeforeAll
     static void configure(){
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
@@ -17,5 +23,6 @@ public class TestBase {
         Attach.screenshotAs("Screenshot name");
         Attach.pageSource("Page source name");
         Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
